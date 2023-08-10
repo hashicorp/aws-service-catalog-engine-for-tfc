da64db2ef7bcce54c869420e8ca3690fc9d96bcf
SwiftEngineer <harbinzerg@gmail.com>
2023-08-10 13:47:23 -0700

    update provider version to version supporting new service catalog product type

--- engine/main.tf
+++ engine/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.63.0"
+      version = "5.12.0"
     }
     tfe = {
       source  = "hashicorp/tfe"
